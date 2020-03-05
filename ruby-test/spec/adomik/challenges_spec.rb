fe9cc2e046372bb6f23864fdb1f1281d7ad5334f
Zhitong Liu <zhitonggm.liu@gmail.com>
2020-03-05 16:33:47 +0100

    Fix Namespace and load dependencies for ruby-test

--- ruby-test/spec/adomik/challenges_spec.rb
+++ ruby-test/spec/adomik/challenges_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Adomik::Ruby::Challenges do
+RSpec.describe Adomik::Challenges do
   it "has a version number" do
-    expect(Adomik::Ruby::Challenges::VERSION).not_to be nil
+    expect(Adomik::Challenges::VERSION).not_to be nil
   end
 
   it "does something useful" do
